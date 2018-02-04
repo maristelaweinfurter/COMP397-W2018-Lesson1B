75cdc02be76924abb0daa0e7fa6e777ae76e1734
MW <maristelaweinfurter@gmail.com>
2018-02-04 03:06:06 -0500

    varias alterações

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -5,22 +5,26 @@
 let canvas = document.getElementById("canvas");
 let stage:createjs.Stage;
 
-  function Init(){
+  function Init():void {
     console.log("Initialization Started...");
     Start();
   }
 
-  function Start(){
+  function Start():void {
     console.log("Starting Application...")
 
     stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60; // 60 fps
+    createjs.Ticker.on("tick",Update);
+    Main();
   }
 
-  function Update() {
-
+  function Update():void {
+      stage.update(); //redraws the stage
   }
 
-  function Main(){
+  function Main():void {
+    console.log("Game Started...");
 
   }
 
