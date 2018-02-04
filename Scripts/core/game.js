ee6a95eceed28a1c82a0e3cad196a35ca4bcb58c
MW <maristelaweinfurter@gmail.com>
2018-02-04 03:06:06 -0500

    Alterações typescript

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,14 +1,24 @@
 // IIFE  Immediately Invoked Function Expression
 (function () {
+    // Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
     function Init() {
-        console.log("Initialization Started");
-        var myVariable = "foo";
+        console.log("Initialization Started...");
+        Start();
     }
     function Start() {
+        console.log("Starting Application...");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 fps
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
     function Update() {
+        stage.update(); //redraws the stage
     }
     function Main() {
+        console.log("Game Started...");
     }
     window.onload = Init;
 })();
